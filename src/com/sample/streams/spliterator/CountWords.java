bf96a50252ecc4311279778996a4a9081ba1e9ff
Swagat Pattnaik <swagatpattnaik@gmail.com>
2021-01-02 19:02:23 -0500

    demo of spliterator 2

--- src/com/sample/streams/spliterator/CountWords.java
+++ src/com/sample/streams/spliterator/CountWords.java
@@ -1,7 +1,9 @@
 package com.sample.streams.spliterator;
 
+import java.util.Spliterator;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class CountWords {
     public static final String SENTENCE= " Nel mezzo del cammin di nostra vita " +
@@ -13,9 +15,15 @@ public class CountWords {
         // Word count iteratively using countWordsIteratively
         System.out.println("Found " + countWords.countWordsIteratively(SENTENCE) + " words using countWordsIteratively()");
 
+        // Word count sequentially using streams
         Stream<Character> stream = IntStream.range(0, SENTENCE.length())
                 .mapToObj(SENTENCE::charAt);
-        System.out.println("Found " + countWords.countWords(stream) + " words using countWords()");
+        System.out.println("Found " + countWords.countWords(stream) + " words using countWords() streams");
+
+        // Word count iteratively using parallel streams
+        Spliterator<Character> spliterator = new WordCounterSpliterator(SENTENCE);
+        Stream<Character> stream2 = StreamSupport.stream(spliterator, true);
+        System.out.println("Found " + countWords.countWords(stream2) + " words using countWords() parallel streams");
     }
 
     private int countWords(Stream<Character> stream) {
